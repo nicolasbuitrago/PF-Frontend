6bd5bac337bbe002a0b4e23e9cc474bc3c237ff3
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2020-02-20 21:13:01 -0500

    Experimentando con Bootstrap carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
