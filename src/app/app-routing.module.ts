ba39e35e5cfe910999516390e79c0a1a627567b1
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2020-02-22 21:08:57 -0500

    Added in-app navigation with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from '../app/components/home/home.component';
+import { StudiosComponent } from '../app/components/studios/studios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'studios', component: StudiosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
