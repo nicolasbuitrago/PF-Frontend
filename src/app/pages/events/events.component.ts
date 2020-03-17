70c6f797842e1c08d9b368231cb93353cf863ebd
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2020-03-17 16:04:08 -0500

    Selected event added

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -11,13 +11,16 @@ import { Event } from 'src/app//models/event.model';
 })
 export class EventsComponent implements OnInit {
 
+  selectedDate: Date;
   today: Date;
   minDate: Date;
   maxDate: Date;
   events: Event[];
+  selectedEvents: Event[];
   dateCustomClasses: DatepickerDateCustomClasses[];
 
   constructor(private eventService: EventService) {
+    this.selectedDate = new Date();
     this.today = new Date();
     this.minDate = new Date();
     this.maxDate = new Date();
@@ -36,9 +39,33 @@ export class EventsComponent implements OnInit {
     this.eventService.getServices()
     .subscribe((events) => {
       this.events = events;
+      this.selectedEvents = [];
       for (const event of events) {
-        this.dateCustomClasses.push({ date:event.date, classes:['bg-secondary', 'text-white']});
+        this.dateCustomClasses.push({ date: event.date, classes: ['bg-secondary', 'text-white']});
+        this.selectedEvents.push(event);
       }
     });
   }
+
+  onDateChange(value: Date): void {
+    this.selectedDate = value;
+    this.selectedEvents = [];
+    console.log('Date changed ' + value.getDay());
+    for (const event of this.events) {
+      console.log(event.date.getTime() + ' === ' + this.selectedDate.getTime());
+      if (this.equalDate(event.date, value)) {
+        console.log('Event added ' + event.title);
+        this.selectedEvents.push(event);
+      }
+    }
+  }
+
+  equalDate(DateA: Date, DateB: Date): boolean {     // this function is good for dates > 01/01/1970
+    const a = new Date(DateA);
+    const b = new Date(DateB);
+    const msDateA = Date.UTC(a.getFullYear(), a.getMonth() + 1, a.getDate());
+    const msDateB = Date.UTC(b.getFullYear(), b.getMonth() + 1, b.getDate());
+    console.log(msDateA + ' === ' + msDateB);
+    return msDateA === msDateB;
+}
 }
