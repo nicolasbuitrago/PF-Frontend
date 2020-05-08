ed90d996066874a0b3f3714f89bb6a35e8732428
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2020-05-08 17:35:09 -0500

    Updated services

--- src/app/shared/interfaces/service.model.ts
+++ src/app/shared/interfaces/service.model.ts
@@ -2,9 +2,9 @@ import { DataComponent } from './data-component.model';
 
 export interface Service extends DataComponent {
   id: number;
-  title: string;
+  name: string;
   link: string;
-  icon: string;
+  image_url: string;
   description: string;
   created_at: Date;
   updated_at: Date;
