281a0dcb76094ef39e2fa6a9c3492dfad4458c6d
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2020-05-19 21:13:02 -0500

    Updated spinner and videogames

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -12,6 +12,7 @@ export class FooterComponent implements OnInit {
 
   title: string;
   content: FooterInformation;
+  year = new Date().getFullYear();
 
   constructor(
     private contentService: ContentService,
